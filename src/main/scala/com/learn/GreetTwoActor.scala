6739dfbf930a3ed67abdcba1d13d7a556d5044d2
Bharateshwar <patilbharateshwar@gmail.com>
2021-01-23 16:10:01 +0530

    Create GreetTwoActor.scala

---
+++ src/main/scala/com/learn/GreetTwoActor.scala
@@ -0,0 +1,20 @@
+package com.learn
+
+
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior}
+
+object HelloWorld {
+  def apply(): Behavior[String] = Behaviors.receive[String] { (context, message) =>
+    print(s"Hello $message")
+    context.log.info("Hello {}!", message)
+    Behaviors.same
+  }
+
+  def main(args: Array[String]): Unit = {
+    ActorSystem(HelloWorld.apply(),"Greet")!(s" World!!");
+  }
+
+
+
+}
